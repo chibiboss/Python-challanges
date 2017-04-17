f8f31cf084245d90d0aea46a4f566caa8880418f
chibiboss <chibibossr@gmail.com>
2017-04-17 12:42:04 -0500

    Tres :3

--- Level_1/level1_ex3.py
+++ Level_1/level1_ex3.py
@@ -8,3 +8,20 @@ Use __init__ method to construct some parameters
 
 
 """
+
+class Palabra:
+	def __init__(self, letras):
+		self.letras = letras
+
+	def getString(self):			
+		texto = input("introduce texto: ")
+		self.letras = texto
+		return texto
+	
+	def printString(self):
+		return self.letras.upper()
+
+if __name__== '__main__':
+	a = Palabra("")
+	a.getString()
+	print (a.printString())
